efb06ef7dc0bd64f3654e67ea3fb0aeb4154c45c
Candace Wilson <candaceyw@gmail.com>
2021-06-28 21:11:37 -0400

    errors cleared

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
-const url = "https://jsonplaceholder.typicode.com/todos/1"
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response => {
-  const todo = response.data;
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const ID = todo.id;
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
-  console.log(`
-  The Todo with ID: ${ID}
-  Has a title of: ${title}
-  Is it finished? ${finished}
-  `);
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${title}
+  Is it completed? ${completed}
+  `);
+};
